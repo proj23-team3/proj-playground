d13c983e75f4977f19f9ac171472e7a09b0cef58
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-03-11 18:27:47 +0100

    initial commit

---
+++ database/migrations/2021_03_11_162800_create_dishes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('ingredients');
+            $table->float('price');
+            $table->boolean('visible')->default(true);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
